0424efb2dafc446d07d66abc9ba197dfcbb95413
hrgweb <gamayonhergenroque.web@gmail.com>
2021-01-28 22:47:31 +0800

    install vue router and register routes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import vuetify from "@/plugins/vuetify"; // path to vuetify export
 import axios from "axios";
 import App from "./App.vue";
+import router from './router'
 
 window.axios = axios;
 
@@ -9,5 +10,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  render: (h) => h(App),
+  router,
+  render: (h) => h(App)
 }).$mount("#app");

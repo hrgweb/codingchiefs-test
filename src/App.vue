7e0b621ff165deb0a76e5a36b763dc5d54097031
hrgweb <gamayonhergenroque.web@gmail.com>
2021-01-28 22:48:24 +0800

    add router view to diplay views or components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
   <div id="app">
-    <Home />
+    <router-view />
   </div>
 </template>
 
-<script>
-import Home from "./components/Home.vue";
-
-export default {
-  name: "App",
-  components: {
-    Home,
-  },
-};
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

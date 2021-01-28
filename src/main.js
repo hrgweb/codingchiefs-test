968cd22df55c81feca56204c67741ba505752733
hrgweb <gamayonhergenroque.web@gmail.com>
2021-01-29 00:36:28 +0800

    install vuex and register

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from "vue";
+import router from "./router";
 import vuetify from "@/plugins/vuetify"; // path to vuetify export
 import axios from "axios";
+import store from "./store/store";
+
 import App from "./App.vue";
-import router from './router'
 
 window.axios = axios;
+window.store = store;
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   router,
-  render: (h) => h(App)
+  store,
+  render: (h) => h(App),
 }).$mount("#app");

b649d11384ffaa9c54b6b2b958cc0012f99c9373
hrgweb <gamayonhergenroque.web@gmail.com>
2021-01-31 22:45:52 +0800

    create component stat box

---
+++ src/components/shared/StatBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box d-flex flex-column justify-center">
+    <b class="text-h4">{{ stat.base }}</b>
+    <span class="subtitle-h6">{{ stat.name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    stat: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.box {
+  background: #eee;
+  padding: 6px 12px;
+  height: 100px;
+}
+</style>

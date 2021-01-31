d7c463879c4efb6d086a3e0d525eadd4facf71a3
hrgweb <gamayonhergenroque.web@gmail.com>
2021-01-31 19:30:15 +0800

    add methods on class Pokemon weight, abilities, evolutions, moves, aboutUrl, about

--- src/class/Pokemon.js
+++ src/class/Pokemon.js
@@ -5,6 +5,36 @@ class Pokemon {
 
     return src.sprites.other[officialArtwork][frontDefault];
   }
+
+  static weight(val) {
+    return val.weight;
+  }
+
+  static abilities(val) {
+    return val.abilities.map((item) => item.ability.name);
+  }
+
+  static evolutions(val) {
+    return val.forms.map((item) => item.name);
+  }
+
+  static moves(val) {
+    return val.moves.map((item) => item.move.name);
+  }
+
+  static aboutUrl(val) {
+    return val.species.url;
+  }
+
+  static about(val) {
+    const lang = "en";
+
+    let enLanguages = val.flavor_text_entries.filter(
+      (item) => item.language.name === lang
+    );
+
+    return enLanguages.map((item) => item.flavor_text).join("");
+  }
 }
 
 export default Pokemon;
